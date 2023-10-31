acba369b32e46adc299ccf39eb863295fdc7ec94
Donghoon Oh <06-sailor-calmest@icloud.com>
2023-10-31 14:50:21 -0700

    add redirection function to index.html

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,7 @@ function sayHello() {
 function takePicture() {
     $("#selectedPicture").click();
  }
+
+ document.getElementById("searchItem").onclick = function() {
+    location.href = "/itemlist.html";
+ };
