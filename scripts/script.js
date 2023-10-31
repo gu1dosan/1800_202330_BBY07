d01a406a8e5225362c865d5801412d2997d610ce
Radmir Garipov <radmirgaripovrss@gmail.com>
2023-10-31 14:54:36 -0700

    Pushing to stop conflict again

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,12 @@ function sayHello() {
 function takePicture() {
     $("#selectedPicture").click();
  }
+document.getElementById("searchButton").onclick = function () {
+    location.href = "../searchIndex.html";
+}
+document.getElementById("photo").onclick = function () {
+    location.href = "../confirmPhoto.html";
+}
+document.getElementById("searchButtonNav").onclick = function () {
+    location.href = "../itemlist.html";
+}
