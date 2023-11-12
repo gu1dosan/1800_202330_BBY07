1cefb4ad9b078c49c1c3e40eab443896e50eadab
Radmir Garipov <radmirgaripovrss@gmail.com>
2023-11-11 19:19:37 -0800

    Changing card layout and getting the photo to appear. The photo does not work yet.

--- scripts/script.js
+++ scripts/script.js
@@ -43,4 +43,25 @@ var photo = ""
 
 document.getElementById(takePhoto()).src = photo;
 
+// Assuming 'firebase' is already initialized and 'storage' is your Firebase Storage reference
+
+// Function to get image URL from Firebase Storage
+function getImageUrl(imagePath) {
+    // Create a reference to the file we want to download
+    var storageRef = firebase.storage().ref();
+    var imageRef = storageRef.child(imagePath);
+
+    // Get the download URL
+    imageRef.getDownloadURL().then(function(url) {
+        // This can now be downloaded directly
+        var img = document.querySelector('img'); // Replace with a more specific selector as needed
+        img.src = url; // Set the image src to the download URL
+    }).catch(function(error) {
+        // Handle any errors
+        console.error("Error fetching image from Firebase Storage", error);
+    });
+}
+
+// For each item in your Firebase Database, call this function with the appropriate image path
+getImageUrl('path/to/your/image.jpg');
 
