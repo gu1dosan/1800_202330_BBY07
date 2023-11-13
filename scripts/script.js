547ee0a653f6a10e141290cbea89d00aa4bc7fca
Donghoon Oh <06-sailor-calmest@icloud.com>
2023-11-12 16:22:00 -0800

    fix getElementByID not loading problem

--- scripts/script.js
+++ scripts/script.js
@@ -15,61 +15,22 @@
 
 
 
-document.getElementById("searchButtonNav").onclick = function () {
-    var query = document.getElementById('searchBar').value;
-    console.log("query= " + query)
-    if(query){
-        console.log("query= " + query)
-        location.href = "../index.html?search=" + query;
-    } else {
-        location.href = "../index.html";
-    }
-    
-}
-
 function goToDetail() {
     
     window.location.href = "../detailpage.html";
     
 }
 
-function takePhoto() {
-    window.location.href = "../TakePhoto.html";
+// function takePhoto() {
+//     window.location.href = "../TakePhoto.html";
 
-}
+// }
 
 
-var photo = ""
+// var photo = ""
 
-document.getElementById(takePhoto()).src = photo;
+// document.getElementById(takePhoto()).src = photo;
 
 function goToProfile(){
     window.location.href = "../profile.html";
 }
-
-
-console.log("hey");
-
-/*
-Log out function
- */
-document.getElementById("sidebar-profile-logout-button").addEventListener("click" , (evt) => {
-    evt.preventDefault();
-    firebase.auth().signOut().then(() => {
-        console.log("logging out successsfully");
-    }).catch((err) => {
-        console.log(err);
-    });
-});
-
-function ex() {
-firebase.auth().signOut().then(() => {
-    console.log("logging out successsfully");
-}).catch((err) => {
-    console.log(err);
-});
-}
-
-ex();
-
-console.log("hey");
