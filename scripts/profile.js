636909edf086c64b93e13272e4f4ffeb6a8664f7
Donghoon Oh <06-sailor-calmest@icloud.com>
2023-11-21 19:13:17 -0800

    finished achievement function and styling

--- scripts/profile.js
+++ scripts/profile.js
@@ -30,7 +30,7 @@ function populateUserInfo() {
 
                 db.collection("waste").get().then((querySnapshot) => {
                     querySnapshot.forEach((doc) => {
-                        if (doc.data().userID === user.uid){
+                        if (doc.data().userID === user.uid) {
                             numOfPosts++;
                             numOfLikes += doc.data().totalLikes;
                         }
@@ -43,6 +43,8 @@ function populateUserInfo() {
                         console.log('User collection successfully updated with new number of posts and likes.');
                         document.getElementById("likesInput").innerText = numOfLikes;
                         document.getElementById("numOfPostInput").innerText = numOfPosts;
+                        progressBar("likes", numOfLikes);
+                        progressBar("posts", numOfPosts);
                     })
 
                 })
@@ -61,7 +63,7 @@ populateUserInfo();
 function editUserInfo() {
     //Enable the form fields
     document.getElementById('personalInfoFields').disabled = false;
- }
+}
 
 function saveUserInfo() {
     userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
@@ -75,74 +77,74 @@ function saveUserInfo() {
     })
 
     document.getElementById('personalInfoFields').disabled = true;
-} 
+}
 
 
 function displayCardsDynamically(collection) {
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        let cardTemplate = document.getElementById("itemCardTemplate");
-        let where = db.collection(collection);
-        
-        
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            let cardTemplate = document.getElementById("itemCardTemplate");
+            let where = db.collection(collection);
+
+
             db.collection("waste")
-            .get()
-            .then(querySnapshot => {
-                querySnapshot.forEach(doc => {
-                    if (doc.data().userID.includes(user.uid)) {
-                    const title = doc.data().title;
-                    var item = doc;
-                    var name = item.data().title;
-                      var imageUrl = item.data().photo;
-                      let id = item.id;
+                .get()
+                .then(querySnapshot => {
+                    querySnapshot.forEach(doc => {
+                        if (doc.data().userID.includes(user.uid)) {
+                            const title = doc.data().title;
+                            var item = doc;
+                            var name = item.data().title;
+                            var imageUrl = item.data().photo;
+                            let id = item.id;
 
-                      let newcard = cardTemplate.content.cloneNode(true);
-                      newcard.querySelector('.item-card-name').innerHTML = name;
-                      newcard.querySelector('.item-card-image').src = imageUrl;
-                      newcard.querySelector('.item-card-image').onclick = () => goToDetail(id);
-                      newcard.querySelector('.item-card-name').onclick = () => goToDetail(id);
-                      newcard.querySelector('.item-card-see-more').onclick = () => goToDetail(id);
-                      // newcard.querySelector('.item-card-color-band').style.backgroundColor = getBinColor(item.data().bin);
-                      newcard.querySelector('.item-card-color-band').style.color = getBinColor(item.data().bin);
-                      newcard.querySelector('.item-card-color-band').onclick = () => goToDetail(id);
-                      // newcard.querySelector('.item-card-likes').style.color = getBinColor(item.data().bin);
+                            let newcard = cardTemplate.content.cloneNode(true);
+                            newcard.querySelector('.item-card-name').innerHTML = name;
+                            newcard.querySelector('.item-card-image').src = imageUrl;
+                            newcard.querySelector('.item-card-image').onclick = () => goToDetail(id);
+                            newcard.querySelector('.item-card-name').onclick = () => goToDetail(id);
+                            newcard.querySelector('.item-card-see-more').onclick = () => goToDetail(id);
+                            // newcard.querySelector('.item-card-color-band').style.backgroundColor = getBinColor(item.data().bin);
+                            newcard.querySelector('.item-card-color-band').style.color = getBinColor(item.data().bin);
+                            newcard.querySelector('.item-card-color-band').onclick = () => goToDetail(id);
+                            // newcard.querySelector('.item-card-likes').style.color = getBinColor(item.data().bin);
 
-                      let likeIcon = newcard.querySelector('.like-icon');
-                      let disLikeIcon = newcard.querySelector('.dislike-icon');
-                      let likesInput = newcard.getElementById('likesInput');
+                            let likeIcon = newcard.querySelector('.like-icon');
+                            let disLikeIcon = newcard.querySelector('.dislike-icon');
+                            let likesInput = newcard.getElementById('likesInput');
 
-                      likeIcon.addEventListener('click', () => {
-                        incrementLike(id).then(() => {
-                            
-                        });
-                    });
-                    disLikeIcon.addEventListener('click', () => {
-                        incrementDisLike(id).then(() => {
-                            
-                        });
-                    });
-                      let itemRef = db.collection(collection).doc(item.id);
-                      itemRef.onSnapshot((doc) => {
-                          if (doc.exists) {
-                              const itemData = doc.data();
-                              let userHasLiked = itemData.whoLiked && itemData.whoLiked.includes(user.uid);
-                              let userHasDisLiked = itemData.whoDisLiked && itemData.whoDisLiked.includes(user.uid);
-                              likesInput.innerHTML = doc.data().totalLikes;
-                              likeIcon.style['font-variation-settings'] = userHasLiked ? "'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 12" : "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24";
-                              disLikeIcon.style['font-variation-settings'] = userHasDisLiked ? "'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 12" : "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24";
-                          } else {
-                              console.log('Document does not exist');
-                          }
-                          
-                      });
+                            likeIcon.addEventListener('click', () => {
+                                incrementLike(id).then(() => {
 
-                      document.getElementById("items-go-here").appendChild(newcard);
+                                });
+                            });
+                            disLikeIcon.addEventListener('click', () => {
+                                incrementDisLike(id).then(() => {
+
+                                });
+                            });
+                            let itemRef = db.collection(collection).doc(item.id);
+                            itemRef.onSnapshot((doc) => {
+                                if (doc.exists) {
+                                    const itemData = doc.data();
+                                    let userHasLiked = itemData.whoLiked && itemData.whoLiked.includes(user.uid);
+                                    let userHasDisLiked = itemData.whoDisLiked && itemData.whoDisLiked.includes(user.uid);
+                                    likesInput.innerHTML = doc.data().totalLikes;
+                                    likeIcon.style['font-variation-settings'] = userHasLiked ? "'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 12" : "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24";
+                                    disLikeIcon.style['font-variation-settings'] = userHasDisLiked ? "'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 12" : "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24";
+                                } else {
+                                    console.log('Document does not exist');
+                                }
+
+                            });
+
+                            document.getElementById("items-go-here").appendChild(newcard);
                         }
-                        })
                     })
-                }
-            
-        })
+                })
+        }
+
+    })
 };
 displayCardsDynamically("waste");
 function getBinColor(bin) {
@@ -160,22 +162,31 @@ function getBinColor(bin) {
     }
 }
 
+function progressBar(id, num) {
+    const bars = ["info", "warning", "danger"];
+    const MUL_FACTOR = 5;
+    let achivementNum = 1;
 
-// // progressBar("likes", 25);
-// function progressBar(id, num) {
-//     const bars = ["info", "warning", "danger"];
-//     const MUL_FACTOR = 5;
-//     let achivementNum = 1;
+    let ele = document.getElementById(id)
+    let progress = ele.getElementsByClassName("progress-bar");
+    let badge = ele.querySelectorAll("span.badge");
 
+    for (let i = 0; i < progress.length; i++) {
+        let percent = Math.round(num / achivementNum * 100);
+        percent = (percent < 0) ? 0 : percent;
+        percent = Math.min(percent, 100);
 
-    for(let i = 0; i < bars.length; i++) {
-        // let ele = document.getElementById(id)
-        // ele = ele.getElementsByClassName("progress-bar");
-        // ele[i].setAttribute("class", ele[i].getAttribute("class") + "");
-        // if()
-        // ele[i].setAttribute("style", "width: " parseInt(achiven))
-        // console.log(ele[i].getAttribute("class"));
+        progress[i].innerText = percent + "%";
+        progress[i].setAttribute("class", progress[i].getAttribute("class") + "");
+        progress[i].setAttribute("style", "width: " + percent + "%");
+
+        if (percent >= 100) {
+            let newAttr = badge[i].getAttribute("class").replace("secondary", bars[i]);
+            badge[i].setAttribute("class", newAttr);
+        }
+        achivementNum *= MUL_FACTOR;
     }
+}
 
 
 function goToDetail(id) {
